c9bcfad371a55db6217e2b803fe4cc75d66fb922
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-03 12:16:58 -0700

    Formatted with prettier

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import axios from "axios";
 
-import './App.css';
+import "./App.css";
 import FriendsList from "./components/FriendsList";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state ={
-      friends: [],
-    }
+    this.state = {
+      friends: []
+    };
   }
 
-  componentDidMount(){
-    axios.get("http://localhost:5000/friends")
-    .then(res => {
+  componentDidMount() {
+    // visit http://localhost:5000/friends to look at data being requested
+    axios.get("http://localhost:5000/friends").then(res => {
       console.log(res.data);
       this.setState({ friends: res.data });
-    })
+    });
   }
 
   render() {
     return (
       <div className="App">
-      <FriendsList friends={this.state.friends}/>
+        <FriendsList friends={this.state.friends} />
       </div>
     );
   }
--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const FriendsList = props => {
-    return (
-        <div className="FriendsList-container">
-        <h2>List of Friends:</h2>
-        {props.friends.map( friend =>
-            <h3>{friend.name}, {friend.age}</h3>)}
-        </div>
-        
-    );    
+  return (
+    <div className="FriendsList-container">
+      <h2>List of Friends:</h2>
+      {props.friends.map(friend => (
+        <h3>
+          {friend.name}, {friend.age}
+        </h3>
+      ))}
+    </div>
+  );
 };
 
 export default FriendsList;
-
