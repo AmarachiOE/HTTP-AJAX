48c085068201d947f6546ef29575dd3ba96457ba
AmarachiOE <ao.ejiawoko@gmail.com>
2019-04-03 12:14:11 -0700

    Get requested friends data from server. Created FriendsList component, that renders name and age of each friend to webpage.

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FriendsList = props => {
+    return (
+        <div className="FriendsList-container">
+        <h2>List of Friends:</h2>
+        {props.friends.map( friend =>
+            <h3>{friend.name}, {friend.age}</h3>)}
+        </div>
+        
+    );    
+};
+
+export default FriendsList;
+
